8f7d15d82514a7273cf9109247502198a7337dc0
Artyom Siomukha <siomukha91@mail.ru>
2025-06-22 16:37:41 +0300

    Лабораторная 6

---
+++ WebLabs.DAL/Entities/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+namespace WebLabs.DAL.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public byte[] AvatarImage { get; set; }
+    }
+}
